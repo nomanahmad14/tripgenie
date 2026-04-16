0e10baf5c0ef4f25ce1dac466251b5b9cf5e7aa6
Noman Ahmad <noman22221@gmail.com>
2026-04-17 01:11:22 +0530

    frontend basic setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen app-bg px-6 py-6">
+      <h1 className="text-main text-3xl font-bold">
+        TripGenie 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
